eea976104974f483d7a751c6a1547b891f502652
GustavoMedeiros <digital.gustavomedeiros@gmail.com>
2025-04-20 20:18:00 -0300

    Criado métodos pertencentes a classe Input

--- RoboTupiniquimApp/Input.cs
+++ RoboTupiniquimApp/Input.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,122 @@ namespace RoboTupiniquimApp.Console
 {
     internal class Input
     {
+        private void showErrorMessage(string typeError)
+        {
+            System.Console.Clear();
+            System.Console.WriteLine($"\n Erro! A entrada digitada {typeError}.");
+            System.Console.WriteLine(" Aperte Enter para continuar...");
+            System.Console.ReadLine();
+            System.Console.Clear();
+        }
+
+        public int[] dimensionsGridMap()
+        {
+            int[] dimensions = new int[2];
+
+            bool ifExit = false;
+            while (ifExit == false)
+            {
+                System.Console.Write("Escreva as dimensões do espaço que será percorrido pelo o rôbo (X Y): ");
+                string dimensionsGrid = System.Console.ReadLine();
+                string[] splitDimensionsGrid = dimensionsGrid.Split(' ');
+
+                int size = splitDimensionsGrid.Length;
+
+                if (size == 2)
+                {
+                    for (int i = 0; i < size; i++)
+                    {
+                        if (int.TryParse(splitDimensionsGrid[i], out dimensions[i]))
+                        {
+                            if (i == (size - 1))
+                                ifExit = true;
+                        }
+                        else
+                        {
+                            showErrorMessage("não tem um ou mais números inteiros.");
+                            break;
+                        }
+                            
+                    }       
+                }
+                else
+                {
+                    showErrorMessage("não tem dois elementos.");
+                }
+            }
+            return dimensions;
+        }
+
+        public Dictionary<char, int[]> firstPosition()
+        {
+            int[] coordinates = new int[2];
+            Dictionary<char, int[]> firstPositionDict = new Dictionary<char, int[]>();
+
+            bool ifExit = false;
+            while (ifExit == false)
+            {
+                System.Console.Write("Escreva a posição inicial do robô (X Y O): ");
+                string position = System.Console.ReadLine();
+                string[] splitPosition = position.Split(' ');
+
+                int size = splitPosition.Length;
+
+                if (size == 3)
+                {
+                    for (int i = 0; i < size; i++)
+                    {
+                        if (!int.TryParse(splitPosition[i], out coordinates[i]))
+                        {
+                            showErrorMessage("não tem um ou mais números inteiros.");
+                            break;
+                        }   
+                    }
+
+                    char[] directions = ['N', 'E', 'S', 'W'];
+
+                    char orientation = char.Parse(splitPosition[2]);
+                    orientation = Char.ToUpper(orientation);
+
+                    if (Array.IndexOf(directions, orientation) == -1)
+                        showErrorMessage("não tem uma direção válida.");
+                    else
+                        firstPositionDict.Add(orientation, coordinates);
+                }
+                else
+                {
+                    showErrorMessage("não tem três elementos.");
+                }
+            }
+            return firstPositionDict;
+        }
+
+        public string commandsRobot()
+        {
+            string commands = "";
+            bool ifExit = false;
+
+            while (ifExit == false)
+            {
+                System.Console.Write("Escreva em uma única linha os comandos do robô: ");
+                commands = System.Console.ReadLine();
+
+                char[] possibleCommands = ['M', 'N', 'E', 'S', 'W'];
+
+                int commandsSize = commands.Length;
+
+                for (int i = 0; i < commandsSize; i++)
+                {
+                    if (Array.IndexOf(possibleCommands, commands[i]) != -1)
+                    {
+                        if (i == (commandsSize - 1))
+                            ifExit = true;
+                    }
+                    else
+                        showErrorMessage("não possui um ou mais comandos existentes.");
+                }
+            }
+            return commands;
+        }
     }
 }
